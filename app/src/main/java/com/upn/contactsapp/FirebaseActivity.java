2c5720f1fdbaa46697c9c53150b1d720e1c3b4e0
Jamie Guadana <jamieguadana@gmail.com>
2024-10-24 22:42:09 -0500

    REYES DÍAZ LUIS ENRIQUE

--- app/src/main/java/com/upn/contactsapp/FirebaseActivity.java
+++ app/src/main/java/com/upn/contactsapp/FirebaseActivity.java
@@ -1,12 +1,18 @@
 package com.upn.contactsapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.upn.contactsapp.entities.Contact;
 
 import java.util.List;
@@ -19,21 +25,44 @@ public class FirebaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
 
+
         Button btn = findViewById(R.id.btnCreateOnFirebase);
+        EditText etName = findViewById(R.id.editTe);
 
         btn.setOnClickListener(v -> {
             // Write a message to the database
             FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef = database.getReference("contacts");
+            DatabaseReference contactsRef = database.getReference("N00300141");
+            DatabaseReference contacts = contactsRef.child("contacts");
+
+            contactsRef.addValueEventListener(new ValueEventListener(){
+
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot child : snapshot.getChildren()){
+                        Contact c = child.getValue(Contact.class);
+                        contacts.add(c);
+                        Log.i("MAIN_APP", c.uuid);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
 
 
-            Contact c1 = new Contact("Luis", "12345678");
-            c1.uuid = UUID.randomUUID().toString();
-            Contact c2 = new Contact("Miguel", "123456");
-            c2.uuid = UUID.randomUUID().toString();
+//            Contact c1 = new Contact("Luis", "12345678");
+//            c1.uuid = UUID.randomUUID().toString();
+//            Contact c3 = new Contact("Miguel", "123456");
+//            c2.uuid = UUID.randomUUID().toString();
 
-            myRef.child(c1.uuid).setValue(c1);
-            myRef.child(c2.uuid).setValue(c2);
+//            String name = etName.getText().toString();
+//            c3.uuid =UUID.randomUUID().toString();
+//            table.child(c3.uuid).setValue(c3);
+
+//            etName.setText("");
 
         });
 
